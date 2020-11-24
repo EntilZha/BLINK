45e192bb2f0b44e6413199e7ddfe5221313aaf33
Pedro Rodriguez <me@pedro.ai>
2020-11-23 22:34:41 -0500

    api

--- blink/main_api.py
+++ blink/main_api.py
@@ -515,7 +515,7 @@ class EntityLinker:
     def create_app(self):
         app = FastAPI()
 
-        @app.get("/api/")
+        @app.get("/api/entity-link/single")
         async def entity_link(text: str):
             return self.link_text(text)
         
